f789eebe59dfa04b8e719db712861b4316480bdf
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-05-10 22:55:45 +0300

    Update work_with_xlsx_1.py

--- work_with_xlsx_1.py
+++ work_with_xlsx_1.py
@@ -34,6 +34,7 @@ for rownum in range(7, 27):
 print(nmin)
 
 #  вариант два через requests
+
 import requests
 import xlrd
 
@@ -47,4 +48,17 @@ nmin = sh.row_values(6)[2]
 for rownum in range(7, 27):
     temp = sh.row_values(rownum)
     nmin = min(nmin, temp[2])
+print(nmin)
+
+#  вариант три pandas
+
+import pandas as pd  # также необходимо было установить openpyxl
+
+wb = pd.read_excel('https://stepik.org/media/attachments/lesson/245266/tab.xlsx')
+nmin = wb.iloc[6][2]  # получаем доступ к данным по индексу строки и столбца
+
+for rownum in range(7, 27):
+      temp = wb.iloc[rownum]
+      nmin = min(nmin, temp[2])
+
 print(nmin)
