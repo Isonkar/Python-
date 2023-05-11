44a236835a9a5e0c6174baabdb897542cddeac7d
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-05-11 10:38:33 +0300

    Create work_with_xlsx_2.py

---
+++ work_with_xlsx_2.py
@@ -0,0 +1,27 @@
+'''
+Вася планирует карьеру и переезд. Для это составил таблицу, в которой для каждого региона записал зарплаты для разных интересные ему профессий. 
+Таблица доступна по ссылке https://stepik.org/media/attachments/lesson/245267/salaries.xlsx. 
+Выведите название региона с самой высокой медианной зарплатой (медианой называется элемент, стоящий в середине массива после его упорядочивания) 
+и, через пробел, название профессии с самой высокой средней зарплатой по всем регионам. 
+'''
+import xlrd3
+
+wb = xlrd3.open_workbook('salaries.xlsx')
+sheet_names = wb.sheet_names()
+sh = wb.sheet_by_name(sheet_names[0])
+
+dict_s = {}
+dict_j = {}
+
+for index in range(1, 8):
+    dict_s[sh.row_values(index)[0]] = sh.row_values(index)[1:]
+    dict_j[sh.col_values(index)[0]] = sh.col_values(index)[1:]
+
+for region, salaries in dict_s.items():
+    dict_s[region] = sorted(salaries)[3]
+  
+for position, salaries in dict_j.items():
+    dict_j[position] = sum(salaries)/len(salaries)  
+
+print(max(dict_s, key=dict_s.get), max(dict_j, key=dict_j.get))
+
