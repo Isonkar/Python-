a13631197ba48c03affbe45b0c18d044fec03acc
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-05-11 10:51:20 +0300

    Update work_with_xlsx_2.py

--- work_with_xlsx_2.py
+++ work_with_xlsx_2.py
@@ -6,22 +6,25 @@
 '''
 import xlrd3
 
+
 wb = xlrd3.open_workbook('salaries.xlsx')
 sheet_names = wb.sheet_names()
 sh = wb.sheet_by_name(sheet_names[0])
-
-dict_s = {}
-dict_j = {}
-
+dict_s = {}, dict_j = {}
 for index in range(1, 8):
     dict_s[sh.row_values(index)[0]] = sh.row_values(index)[1:]
     dict_j[sh.col_values(index)[0]] = sh.col_values(index)[1:]
-
 for region, salaries in dict_s.items():
     dict_s[region] = sorted(salaries)[3]
-  
-for position, salaries in dict_j.items():
+ for position, salaries in dict_j.items():
     dict_j[position] = sum(salaries)/len(salaries)  
-
 print(max(dict_s, key=dict_s.get), max(dict_j, key=dict_j.get))
+
+
+#  вариант два с pandas
+import pandas as pd
+
+
+data = pd.read_excel('salaries.xlsx', index_col=0)
+print(data.median(axis=1).idxmax(), data.mean(axis=0).idxmax())
 
